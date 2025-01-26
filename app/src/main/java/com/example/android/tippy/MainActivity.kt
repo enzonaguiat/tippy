72c55b603de29fb4805c6e24dc2d3426e29345c2
enzonaguiat <enzonaguiat@protonmail.com>
2025-01-26 14:07:57 +0900

    app now updates tip amount and total amount when base amount or tip percent seekbar are adjusted. Also added a check for base amount that is null or empty.

--- app/src/main/java/com/example/android/tippy/MainActivity.kt
+++ app/src/main/java/com/example/android/tippy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.tippy
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
@@ -34,19 +36,44 @@ class MainActivity : AppCompatActivity() {
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
 
-        seekBarTip.progress = "$INITIAL_TIP_PERCENT%"
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
         seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged: $p1")
                 tvTipPercent.text = "$p1%"
+                computeTipAndTotal()
             }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
 
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
 
         })
+        etBaseAmount.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "after text change: $p0")
+                computeTipAndTotal()
+            }
+        })
+    }
+
+    private fun computeTipAndTotal() {
+        if (etBaseAmount.text.isEmpty()){
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        var baseAmount = etBaseAmount.text.toString().toDouble()
+        var tipPercent = seekBarTip.progress
+        var tipAmount = baseAmount * tipPercent / 100
+        var totalAmount = baseAmount + tipAmount
+
+        tvTipAmount.text = tipAmount.toString()
+        tvTotalAmount.text = totalAmount.toString()
     }
 }
